a699d17308c6ead596d4f7b5d82b9b8a36952e28
Cronwel Noel Irias <noelirias@gmail.com>
2017-12-14 14:29:14 -0600

    Front-facing pages created

---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MainControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get main_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get main_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get main_contact_url
+    assert_response :success
+  end
+
+end
