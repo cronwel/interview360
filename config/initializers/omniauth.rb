cd1b00b19d97c72402f3a75fb2e7b61b7a3d5fa5
Cronwel Noel Irias <noelirias@gmail.com>
2017-12-17 15:14:32 -0600

    Procfile created

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,3 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, 'Google Client', 'secret',
   {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
+
+
+# alternative method
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,19 @@
 Rails.application.routes.draw do
+
   get 'auth/:provider/callback', to: 'session#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  
   resources :sessions, only: [:create, :destroy]
-  # resource :home, only:[:show]
+  resource :home, only:[:show]
 
   get 'sessions/create'
-
   get 'sessions/destroy'
 
-  get 'main/home'
-
-  get 'main/about'
-
-  get 'main/contact'
-
-  root to: 'main#home'
+  get 'home/about'
+  get 'home/contact'
+  get 'home/home'
+  get 'home/show'
+  root to: 'home#show'
 
 end
-#
-#
-# Rails.application.routes.draw do
-#
-# end
